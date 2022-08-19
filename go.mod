1b7bce7ab57f26ae804328344efbdd080c4cb578
uniters_ngo <uniters-fundation@tutanota.com>
2022-08-20 00:54:42 +0200

    update: started working on basic blog structure

---
+++ go.mod
@@ -0,0 +1,3 @@
+module code_of_fury_blog
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+var tpl *template.Template
+func init() {
+	var err error
+	tpl, err = tpl.ParseGlob("templates/*.gohtml")
+
+	if err != nil {
+		log.Panicln(err)
+	}
+}
+
+func main() {
+	http.HandleFunc("/", homePageRoute)
+
+	log.Fatal(http.ListenAndServe(":9090", nil))
+}
+
+type post struct{Title string; URL string}
+type postListing []post
+type pageData struct {
+	Title string;
+	Posts postListing;
+}
+
+func homePageRoute(w http.ResponseWriter, r *http.Request){
+	data := pageData{
+		Title: "Code of Fury - Index",
+		Posts: postListing{
+			post{Title: "Hello post", URL: "/post/hello"},
+			post{Title: "About go", URL: "/post/about-go"},
+		},
+	}
+	err := tpl.ExecuteTemplate(w, "index.gohtml", data)
+
+	if err != nil {
+		log.Panicln(err)
+	}
+}
